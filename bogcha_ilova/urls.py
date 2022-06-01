d7dd6ebfc8548647795ddbdbf57d54bcbd279d5f
kazakovasitora <kazakovasitora06@gmail.com>
2022-06-01 19:34:54 +0500

    biz boshladik

---
+++ bogcha_ilova/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', bosh_sahifa, name='home'),
+    path('about/', about, name='about'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+    path('', bosh_sahifa, name='home'),
+]
