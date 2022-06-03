3acbb0c89550e24ff8cd4a77bf2f8ac54f07ec8e
kazakovasitora <kazakovasitora06@gmail.com>
2022-06-03 18:42:55 +0500

    model & model_views & photo_url & media & html_for

--- bogcha_ilova/models.py
+++ bogcha_ilova/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Lesson(models.Model):
+    img = models.ImageField(upload_to='Darslarning_rasmi')
+    dars_nomi = models.CharField(max_length=10)
+    malumoti = models.TextField()
+    bolalarning_yoshi = models.IntegerField(default=1)
+    joy_soni = models.IntegerField(default=5)
+    dars_vaqti = models.DateTimeField()
+    dars_narxi = models.IntegerField(default=0)
+
+    #telegram = models.URLField()
+    #facebook = models.URLField()
+    #instagram = models.URLField()
+    
+    def __str__(self):
+        return self.dars_nomi
+    
+
--- bogcha_ilova/views.py
+++ bogcha_ilova/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-
+from .models import Lesson
 # Create your views here.
 
 def bosh_sahifa(request):
-    return render(request, 'index.html')
+    darslar = Lesson.objects.all()
+    return render(request, 'index.html', {"Html_Dars": darslar})
 def about(request):
     return render(request, 'about.html')
 def blok(request):
     return render(request, 'blog.html')
 def klas(request):
-    return render(request, 'class.html')
+    darslar = Lesson.objects.all()
+    return render(request, 'class.html', {"Html_Dars": darslar})
 def kontakt(request):
     return render(request, 'contact.html')
 def galery(request):
