246ac34f05863ccdc0b78ccbd55cea5693363228
kazakovasitora <kazakovasitora06@gmail.com>
2022-06-03 17:48:39 +0500

    probel qo`yib chiqdim

--- bogcha_ilova/urls.py
+++ bogcha_ilova/urls.py
@@ -4,12 +4,10 @@ from .views import *
 urlpatterns = [
     path('', bosh_sahifa, name='home'),
     path('about/', about, name='about'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
-    path('', bosh_sahifa, name='home'),
+    path('blok/', blok, name='blok'),
+    path('klas/', klas, name='class'),
+    path('kontakt/', kontakt, name='contakt'),
+    path('gallery/', galery, name='gallery'),
+    path('sing/', sing, name='single'),
+    path('team/', team1, name='team'),
 ]
--- bogcha_ilova/views.py
+++ bogcha_ilova/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render
 
 def bosh_sahifa(request):
     return render(request, 'index.html')
-
 def about(request):
     return render(request, 'about.html')
+def blok(request):
+    return render(request, 'blog.html')
+def klas(request):
+    return render(request, 'class.html')
+def kontakt(request):
+    return render(request, 'contact.html')
+def galery(request):
+    return render(request, 'gallery.html')
+def sing(request):
+    return render(request, 'single.html')
+def team1(request):
+    return render(request, 'team.html')
